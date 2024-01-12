2c49efd0307f51ed5c59ef633c3ee8cd9b128c77
AA1HSHH <974658390@qq.com>
2024-01-12 16:58:39 +0800

    v1

---
+++ README.md
@@ -0,0 +1,20 @@
+# NJUPT自动连接脚本
+## 环境安装
+```
+conda create -n net python=3.8
+conda activate net
+pip install -i https://pypi.tuna.tsinghua.edu.cn/simple psutil requests 
+```
+## 执行
+第一个参数是conda环境的python位置，第二个参数是文件位置
+```
+D:/Program/miniconda3/envs/net/python.exe e:/code/py/auto_con/net.py
+```
+## 定时执行
+
+## 致谢
+GPT3.5
+
+https://github.com/Yolumia/NJUPT-NETWORK
+
+
---
+++ net.py
@@ -0,0 +1,39 @@
+import os
+import socket
+import psutil
+import requests
+if __name__ == "__main__":
+    # 连接NJUPT-CMCC WLAN
+    os.system("netsh wlan connect name=NJUPT-CMCC")
+    # 处理 NameResolutionError ailed to resolve 'p.njupt.edu.cn'
+    os.system("nslookup p.njupt.edu.cn")
+
+    # 获取ip地址
+    net_interfaces = psutil.net_if_addrs()
+    wlan_ip = None
+    for interface, addresses in net_interfaces.items():
+        if interface == "WLAN":
+            wlan_ip = [addr.address for addr in addresses if addr.family == socket.AF_INET][0]
+            break
+    if wlan_ip:
+        print(f"WLAN IPv4 Address: {wlan_ip}")
+    else:
+        print("WLAN IPv4 Address not found.")
+        os.exit(1)
+
+    # 构造请求体
+    user = "name"
+    passwd = "pass"
+    url = "https://p.njupt.edu.cn:802/eportal/portal/login?callback=dr1003&login_method=1&user_account=%2C0%2C"+user+"%40cmcc&user_password="+passwd+"&wlan_user_ip="+wlan_ip+"&wlan_user_ipv6=&wlan_user_mac=000000000000&wlan_ac_ip=&wlan_ac_name=&jsVersion=4.1.3&terminal_type=1&lang=zh-cn&v=3140&lang=zh"
+    
+    try:
+        response = requests.get(url)
+
+        # 检查响应状态码
+        if response.status_code == 200:
+            # 打印响应内容
+            print("连接成功")
+        else:
+            print(f"失败: {response.status_code} - {response.reason}")
+    except Exception as e:
+        print(f"Error: {e}")
